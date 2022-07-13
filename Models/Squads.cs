8a359bcd71bc3d3bb59f867124f07e3d3d5d4c88
Michel Jurado <104876738+MichelJurado@users.noreply.github.com>
2022-07-13 09:00:03 -0400

    Add files via upload

---
+++ Models/Squads.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedJSON2.Models
+{
+
+    public class Rootobject1
+    {
+        public string get { get; set; }
+        public Parameters parameters { get; set; }
+        public object[] errors { get; set; }
+        public int results { get; set; }
+        public Paging paging { get; set; }
+        public Response1[] response { get; set; }
+    }
+
+    public class Parameters1
+    {
+        public string team { get; set; }
+    }
+
+    public class Paging1
+    {
+        public int current { get; set; }
+        public int total { get; set; }
+    }
+
+    public class Response1
+    {
+        public Team1 team { get; set; }
+        public Player1[] players { get; set; }
+    }
+
+    public class Team1
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string logo { get; set; }
+    }
+
+    public class Player1
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int? number { get; set; }
+        public string position { get; set; }
+        public string photo { get; set; }
+    }
+
+
+}
---
+++ Models/Trophies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedJSON2.Models
+{
+
+
+    public class Rootobject2
+    {
+        
+        public Parameters2 parameters { get; set; }
+        
+        
+        
+        public Response2[] response { get; set; }
+    }
+
+    public class Parameters2
+    {
+        public string player { get; set; }
+    }
+
+    
+
+    public class Response2
+    {
+        public string league { get; set; }
+        public string country { get; set; }
+        public string season { get; set; }
+        public string place { get; set; }
+    }
+
+
+}
